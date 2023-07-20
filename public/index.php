e65cdb60d669066db4cdbc7679d9fef63572daf0
Brandon Ho <b432ho@vt.edu>
2023-07-19 22:01:30 -0400

    added comment to index.php

--- public/index.php
+++ public/index.php
@@ -9,3 +9,9 @@ define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 /* YOUR CODE (Instructions in README.md) */
+
+/*
+ req'd files
+    ...
+    ...S
+*/
