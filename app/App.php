1fc6045f9dc45dfed5dd2e56eddcff5a825157d3
Brandon Ho <b432ho@vt.edu>
2023-07-26 12:23:43 -0400

    - new func read_transactions - new file temp_sample_2.csv

--- app/App.php
+++ app/App.php
@@ -3,3 +3,30 @@
 declare(strict_types = 1);
 
 // Your Code
+
+/*
+    reads files in transactions directory
+
+    FUTURE IMPROVEMENTS:
+    - $file var optional param for reading specific files separated by comma,
+      empty = all files
+*/
+function read_transactions(String $file = ""): void {
+    /* NOT IMPLEMENTED
+    ~~~~~
+    if (empty($file)) {
+        
+    } else {
+        $file = explode(',', $file);
+    }
+    ~~~~~
+    */
+    $input_file_dir = FILES_PATH.'sample_1.csv';
+    if (!file_exists($input_file_dir)) {
+        echo "File does not exist.";
+        return;
+    }
+
+    $file = fopen($input_file_dir, 'r');
+    echo "Success!";
+}
--- public/index.php
+++ public/index.php
@@ -10,8 +10,20 @@ define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 /* YOUR CODE (Instructions in README.md) */
 
+echo $root;
+echo nl2br("\n\n");
+echo APP_PATH;
+echo nl2br("\n\n");
+echo FILES_PATH;
+echo nl2br("\n\n");
+echo VIEWS_PATH;
+echo nl2br("\n\n");
+
 /*
  req'd files
+    App.php - includes logic functions
     ...
-    ...S
 */
+require_once(APP_PATH.'App.php');
+
+read_transactions();
